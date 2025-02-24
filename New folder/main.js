10667e5a4b04e0cb830d73e83a9395293d2e8ceb
ENG ABDINASER <cabdinaasircabdirashiid18@gmail.com>
2025-02-24 11:41:21 +0300

    Add files via upload

---
+++ New folder/main.js
@@ -0,0 +1,31 @@
+let heading = documentGetElementById("heading");
+console.log(heading)
+
+
+let studentName = {
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//string
+///num
+//bolean
+//undefimced
+//null
+//array
+//funcation
